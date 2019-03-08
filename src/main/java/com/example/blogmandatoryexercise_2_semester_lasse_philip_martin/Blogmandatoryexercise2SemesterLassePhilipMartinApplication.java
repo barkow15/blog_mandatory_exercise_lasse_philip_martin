16b2520cbabfb3bc856f213e0a8151c99376a06a
Philip Thomas Barkow <barkow15@gmail.com>
2019-03-08 11:23:56 +0100

    Initialiseret github repo og oprettet projektet

---
+++ src/main/java/com/example/blogmandatoryexercise_2_semester_lasse_philip_martin/Blogmandatoryexercise2SemesterLassePhilipMartinApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogmandatoryexercise_2_semester_lasse_philip_martin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogmandatoryexercise2SemesterLassePhilipMartinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Blogmandatoryexercise2SemesterLassePhilipMartinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/blogmandatoryexercise_2_semester_lasse_philip_martin/Blogmandatoryexercise2SemesterLassePhilipMartinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.blogmandatoryexercise_2_semester_lasse_philip_martin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Blogmandatoryexercise2SemesterLassePhilipMartinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
